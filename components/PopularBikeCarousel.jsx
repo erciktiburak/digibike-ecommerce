8c6be5893c3aa8ebe9002141a93f0cda2887cc32
Burak Erçıktı <burakercikti@outlook.com>
2024-01-27 21:46:11 +0300

    Carousel Edited

--- components/PopularBikeCarousel.jsx
+++ components/PopularBikeCarousel.jsx
@@ -1,8 +1,26 @@
 'use client';
+import Bike from './Bike';
+
+//import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 const PopularBikeCarousel = ({bikes}) => {
-  console.log(bikes)
-  return <div>Popular Bike Carousel</div>
+  return (
+    <Swiper>
+    {bikes.map((bike) => {
+      return <SwiperSlide key={bike._id}>
+        <Bike bike={bike}/>
+      </SwiperSlide>
+    })}
+    </Swiper>
+    )
 }
 
-export default PopularBikeCarousel
+export default PopularBikeCarousel;
